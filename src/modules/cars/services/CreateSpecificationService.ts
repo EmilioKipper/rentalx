d55f152a988b65c00b38cf032b1f8a9a0813c608
Emílio Gularte Kipper <emilio.kipper@programmers.com.br>
2023-01-11 13:48:21 -0300

    Create modules

---
+++ src/modules/cars/services/CreateSpecificationService.ts
@@ -0,0 +1,18 @@
+// interface IRequest {
+//   name: string;
+//   description: string;
+// }
+
+class CreateSpecificationService {
+  // constructor(private categoriesRepository: ICategoryRepository) {}
+
+  execute(): void {
+    // const categoryAlreadyExists = this.categoriesRepository.findByName(name);
+    // if (categoryAlreadyExists) {
+    //   throw new Error("Category already exists!");
+    // }
+    // this.categoriesRepository.create({ name, description });
+  }
+}
+
+export { CreateSpecificationService };
