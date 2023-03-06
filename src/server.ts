8a68e779deb66de2b230bb0d1461e71911991b0e
Emílio Gularte Kipper <emilio.kipper@programmers.com.br>
2023-03-06 08:54:02 -0300

    Add base documentation

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,14 @@
 import express from "express";
+import swaggerUI from "swagger-ui-express";
 
 import { router } from "./routes";
+import swaggerConfig from "./swagger.json";
 
 const app = express();
 
 app.use(express.json());
+
+app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(swaggerConfig));
 
 app.use(router);
 
