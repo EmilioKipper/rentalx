9cf355d264cb3bc0cd2ef2223eb7df168f4b78ce
Emílio Gularte Kipper <emilio.kipper@programmers.com.br>
2023-05-17 14:18:44 -0300

    Code refactor

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -4,14 +4,14 @@ import express, { NextFunction, Request, Response } from "express";
 import "express-async-errors";
 import swaggerUI from "swagger-ui-express";
 
-import "./database";
+import "@shared/infra/typeorm";
 
 import "@shared/container";
 
-import { AppError } from "@errors/AppError";
+import { AppError } from "@shared/errors/AppError";
 
+import swaggerConfig from "../../../swagger.json";
 import { router } from "./routes";
-import swaggerConfig from "./swagger.json";
 
 dotenv.config();
 
