7fbe33fdcd3b7eb8658af17ee8b581f3643e10ea
Emílio Gularte Kipper <emilio.kipper@programmers.com.br>
2023-10-09 15:34:18 -0300

    Change database connection file

--- src/shared/infra/typeorm/index.ts
+++ src/shared/infra/typeorm/index.ts
@@ -1,12 +1,11 @@
 import { Connection, createConnection, getConnectionOptions } from "typeorm";
 
-export default async (host = "database"): Promise<Connection> => {
+export default async (): Promise<Connection> => {
   const defaultOptions = await getConnectionOptions();
   const node_env = process.env.NODE_ENV.trim();
 
   return createConnection(
     Object.assign(defaultOptions, {
-      host: node_env === "test" ? "localhost" : host,
       database: node_env === "test" ? "rentx_test" : defaultOptions.database,
     })
   );
